e51cd1b996f8f32ad30af5d093a112c2a70aba0e
Philippe <philippe.sa.costa@gmail.com>
2026-04-13 16:46:52 +0200

    refactor: standardize SQL type casting and improve formatting
    - Update `int_orders_join_customers` to use `::DATE` for timestamp casting
    - Standardize `customer_zip_code_prefix` casting to `::INTEGER` in `stg_customers`
    - Align column formatting in `stg_order_items` and `stg_orders` for consistency
    - Ensure consistent spacing and alignment for better readability

--- 0503-0504_snowflake_dbt/sf_dbt_project/models/staging/stg_order_items.sql
+++ 0503-0504_snowflake_dbt/sf_dbt_project/models/staging/stg_order_items.sql
@@ -2,10 +2,10 @@ with order_items as (
     select
         order_id
         , product_id
-        , order_item_id::INTEGER as order_item_id
+        , order_item_id::INTEGER                          as order_item_id
         , seller_id
-        , price::DECIMAL(10,2) as price
-        , freight_value::DECIMAL(10,2) as freight_value
+        , price::DECIMAL(10,2)                            as price
+        , freight_value::DECIMAL(10,2)                    as freight_value
         , {{ cast_brt_timestamp('shipping_limit_date') }} as shipping_limit_date
     from {{ source('olist', 'OLIST_ORDER_ITEMS') }}
 )
